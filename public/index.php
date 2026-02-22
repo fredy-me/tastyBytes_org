503bf7e8ec611c50b2e0e7213f6e25a187e33f16
fredy-me <fredymelkizedek@gmail.com>
2026-02-23 01:39:59 +0300

    Implement recipe management routes in index.php, adding functionality for viewing, creating, editing, and deleting recipes, as well as admin actions for user management.

--- public/index.php
+++ public/index.php
@@ -36,9 +36,57 @@ switch ($route) {
         (new \App\Http\Controllers\AuthController())->logout();
         break;
     case 'recipes':
-        \App\Support\Response::abort(501, 'Recipes coming next (Feature C).');
+        (new \App\Http\Controllers\RecipeController())->index();
+        break;
+    case 'recipe':
+        (new \App\Http\Controllers\RecipeController())->show();
+        break;
+    case 'my_recipes':
+        (new \App\Http\Controllers\RecipeController())->myRecipes();
+        break;
+    case 'recipe_create':
+        $c = new \App\Http\Controllers\RecipeController();
+        if (\App\Support\Request::isPost()) {
+            \App\Support\Response::abort(405);
+        }
+        $c->createForm();
+        break;
+    case 'recipe_store':
+        (new \App\Http\Controllers\RecipeController())->create();
+        break;
+    case 'recipe_edit':
+        (new \App\Http\Controllers\RecipeController())->editForm();
+        break;
+    case 'recipe_update':
+        (new \App\Http\Controllers\RecipeController())->update();
+        break;
+    case 'recipe_delete':
+        (new \App\Http\Controllers\RecipeController())->delete();
+        break;
+    case 'favorites':
+        (new \App\Http\Controllers\RecipeController())->favorites();
+        break;
+    case 'favorite_toggle':
+        (new \App\Http\Controllers\RecipeController())->favoriteToggle();
+        break;
+    case 'admin':
+        (new \App\Http\Controllers\AdminController())->dashboard();
+        break;
+    case 'admin_approve':
+        (new \App\Http\Controllers\AdminController())->approve();
+        break;
+    case 'admin_reject':
+        (new \App\Http\Controllers\AdminController())->reject();
+        break;
+    case 'admin_users':
+        (new \App\Http\Controllers\AdminController())->users();
+        break;
+    case 'admin_user_disable':
+        (new \App\Http\Controllers\AdminController())->disableUser();
+        break;
+    case 'admin_user_delete':
+        (new \App\Http\Controllers\AdminController())->deleteUser();
         break;
     default:
         \App\Support\Response::abort(404, 'Not found.');
 }
-
